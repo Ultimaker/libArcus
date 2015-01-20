ca948ba27640fba256ded88763843f8959f92ae8
Arjen Hiemstra <ahiemstra@heimr.nl>
2015-01-20 17:05:46 +0100

    Update python example to send multiple messages

--- examples/example.py
+++ examples/example.py
@@ -1,23 +1,16 @@
 import example_pb2
 import Arcus
 
+import time
+
 socket = Arcus.Socket()
 
 socket.registerMessageType(2, example_pb2.ObjectList)
 socket.registerMessageType(5, example_pb2.ProgressUpdate)
 socket.registerMessageType(6, example_pb2.SlicedObjectList)
 
-def onStateChanged():
-    if socket.getState() == Arcus.Socket.ConnectedState:
-        print('sending objects')
-        msg = example_pb2.ObjectList()
-        for i in range(10):
-            obj = msg.objects.add()
-            obj.id = i
-            obj.vertices = b'abcdefghijklmnopqrstuvwxyz'
-            obj.normals = b'abcdefghijklmnopqrstuvwxyz'
-            obj.indices = b'abcdefghijklmnopqrstuvwxyz'
-        socket.sendMessage(msg)
+def onStateChanged(newState):
+    print('State Changed:', newState)
 
 def onMessageAvailable():
     message = socket.takeNextMessage()
@@ -34,3 +27,18 @@ def onMessageAvailable():
 socket.setStateChangedCallback(onStateChanged)
 socket.setMessageAvailableCallback(onMessageAvailable)
 socket.listen('127.0.0.1', 56789)
+
+time.sleep(10)
+
+for i in range(10):
+    msg = example_pb2.ObjectList()
+    for i in range(10):
+        obj = msg.objects.add()
+        obj.id = i
+        obj.vertices = b'abcdefghijklmnopqrstuvwxyz'
+        obj.normals = b'abcdefghijklmnopqrstuvwxyz'
+        obj.indices = b'abcdefghijklmnopqrstuvwxyz'
+    socket.sendMessage(msg)
+    time.sleep(5)
+
+socket.close()
