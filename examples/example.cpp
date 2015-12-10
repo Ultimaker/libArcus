14e5021220336d26457ecbc70e2b9c2e1db91d58
Ghostkeeper <ghost_keeper+github@hotmail.com>
2015-12-10 11:53:28 +0100

    Codestyle fix
    I think this was an automated search-and-replace that went wrong.

--- examples/example.cpp
+++ examples/example.cpp
@@ -39,7 +39,7 @@ int main(int argc, char** argv)
             {
                 handleMessage(socket, message);
             }
-            std::this_thread::sleep_for (std::chrono::milliseconds(250));
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
         }
         else if (socket.state() == Arcus::SocketState::Closed || socket.state() == Arcus::SocketState::Error)
         {
