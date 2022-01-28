6e92793d184f6a8bb1a8602895480756269a6d9a
Jaime van Kessel <nallath@gmail.com>
2022-01-28 15:06:30 +0100

    Update the SIP cmake scripts

--- cmake/FindSIP.cmake
+++ cmake/FindSIP.cmake
@@ -8,85 +8,58 @@
 #
 # This file defines the following variables:
 #
-# SIP_VERSION - SIP version.
+# SIP_VERSION - The version of SIP found expressed as a 6 digit hex number
+#     suitable for comparison as a string.
 #
-# SIP_EXECUTABLE - Path to the SIP executable.
+# SIP_VERSION_STR - The version of SIP found as a human readable string.
 #
-# SIP_INCLUDE_DIRS - The SIP include directories.
+# SIP_BINARY_PATH - Path and filename of the SIP command line executable.
 #
+# SIP_INCLUDE_DIR - Directory holding the SIP C++ header file.
+#
+# SIP_DEFAULT_SIP_DIR - Default directory where .sip files should be installed
+#     into.
 
 # Copyright (c) 2007, Simon Edwards <simon@simonzone.com>
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-if(APPLE)
-    # Workaround for broken FindPythonLibs. It will always find Python 2.7 libs on OSX
-    set(CMAKE_FIND_FRAMEWORK LAST)
-endif()
 
-# FIXME: Use FindPython3 to find Python, new in CMake 3.12.
-# However currently on our CI server it finds the wrong Python version and then doesn't find the headers.
-find_package(PythonInterp 3.5 REQUIRED)
-find_package(PythonLibs 3.5 REQUIRED)
 
-# Define variables that are available in FindPython3, so there's no need to branch off in the later part.
-set(Python3_EXECUTABLE ${PYTHON_EXECUTABLE})
-set(Python3_INCLUDE_DIRS ${PYTHON_INCLUDE_DIRS})
-set(Python3_LIBRARIES ${PYTHON_LIBRARIES})
-set(Python3_VERSION_MINOR "${PYTHON_VERSION_MINOR}")
+IF(SIP_VERSION)
+  # Already in cache, be silent
+  SET(SIP_FOUND TRUE)
+ELSE(SIP_VERSION)
 
-execute_process(
-   COMMAND ${Python3_EXECUTABLE} -c
-           "import distutils.sysconfig; print(distutils.sysconfig.get_python_lib(plat_specific=False,standard_lib=False))"
-   RESULT_VARIABLE _process_status
-   OUTPUT_VARIABLE _process_output
-   OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-if(${_process_status} EQUAL 0)
-   string(STRIP ${_process_output} Python3_SITELIB)
-else()
-   message(FATAL_ERROR "Failed to get Python3_SITELIB. Error: ${_process_output}")
-endif()
+  FIND_FILE(_find_sip_py FindSIP.py PATHS ${CMAKE_MODULE_PATH} NO_CMAKE_FIND_ROOT_PATH)
 
-execute_process(
-   COMMAND ${Python3_EXECUTABLE} -c
-           "import distutils.sysconfig; print(distutils.sysconfig.get_python_lib(plat_specific=True,standard_lib=False))"
-   RESULT_VARIABLE _process_status
-   OUTPUT_VARIABLE _process_output
-   OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-if(${_process_status} EQUAL 0)
-   string(STRIP ${_process_output} Python3_SITEARCH)
-else()
-   message(FATAL_ERROR "Failed to get Python3_SITEARCH. Error: ${_process_output}")
-endif()
+  EXECUTE_PROCESS(COMMAND ${Python_EXECUTABLE} ${_find_sip_py} OUTPUT_VARIABLE sip_config)
+  IF(sip_config)
+    STRING(REGEX REPLACE "^sip_version:([^\n]+).*$" "\\1" SIP_VERSION ${sip_config})
+    STRING(REGEX REPLACE ".*\nsip_version_num:([^\n]+).*$" "\\1" SIP_VERSION_NUM ${sip_config})
+    STRING(REGEX REPLACE ".*\nsip_version_str:([^\n]+).*$" "\\1" SIP_VERSION_STR ${sip_config})
+    STRING(REGEX REPLACE ".*\ndefault_sip_dir:([^\n]+).*$" "\\1" SIP_DEFAULT_SIP_DIR ${sip_config})
+    IF(${SIP_VERSION_STR} VERSION_LESS 5)
+      STRING(REGEX REPLACE ".*\nsip_bin:([^\n]+).*$" "\\1" SIP_BINARY_PATH ${sip_config})
+      STRING(REGEX REPLACE ".*\nsip_inc_dir:([^\n]+).*$" "\\1" SIP_INCLUDE_DIR ${sip_config})
+      STRING(REGEX REPLACE ".*\nsip_module_dir:([^\n]+).*$" "\\1" SIP_MODULE_DIR ${sip_config})
+    ELSE(${SIP_VERSION_STR} VERSION_LESS 5)
+      FIND_PROGRAM(SIP_BUILD_EXECUTABLE sip-build)
+    ENDIF(${SIP_VERSION_STR} VERSION_LESS 5)
+    SET(SIP_FOUND TRUE)
+  ENDIF(sip_config)
 
-get_filename_component(_python_binary_path ${Python3_EXECUTABLE} DIRECTORY)
+  IF(SIP_FOUND)
+    IF(NOT SIP_FIND_QUIETLY)
+      MESSAGE(STATUS "Found SIP version: ${SIP_VERSION_STR}")
+    ENDIF(NOT SIP_FIND_QUIETLY)
+  ELSE(SIP_FOUND)
+    IF(SIP_FIND_REQUIRED)
+      MESSAGE(FATAL_ERROR "Could not find SIP")
+    ENDIF(SIP_FIND_REQUIRED)
+  ENDIF(SIP_FOUND)
 
-find_program(SIP_EXECUTABLE sip
-    HINTS ${CMAKE_PREFIX_PATH}/bin ${CMAKE_INSTALL_PATH}/bin ${_python_binary_path} ${Python3_SITELIB}/PyQt5
-)
+ENDIF(SIP_VERSION)
 
-find_path(SIP_INCLUDE_DIRS sip.h
-    HINTS ${CMAKE_PREFIX_PATH}/include ${CMAKE_INSTALL_PATH}/include ${Python3_INCLUDE_DIRS} ${Python3_SITELIB}/PyQt5
-)
+ADD_DEFINITIONS(-DSIP_VERSION=0x${SIP_VERSION})
 
-execute_process(
-    COMMAND ${Python3_EXECUTABLE} -c "import sip; print(sip.SIP_VERSION_STR)"
-    RESULT_VARIABLE _process_status
-    OUTPUT_VARIABLE _process_output
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-
-if(${_process_status} EQUAL 0)
-    string(STRIP ${_process_output} SIP_VERSION)
-endif()
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SIP REQUIRED_VARS SIP_EXECUTABLE SIP_INCLUDE_DIRS VERSION_VAR SIP_VERSION)
-
-if(SIP_FOUND)
-    include(${CMAKE_CURRENT_LIST_DIR}/SIPMacros.cmake)
-endif()
-
-mark_as_advanced(SIP_EXECUTABLE SIP_INCLUDE_DIRS SIP_VERSION)
--- cmake/SIPMacros.cmake
+++ cmake/SIPMacros.cmake
@@ -16,10 +16,10 @@
 #     which is typically a shared library, should be linked to. The built
 #     module will also be install into Python's site-packages directory.
 #
-# The behaviour of the ADD_SIP_PYTHON_MODULE macro can be controlled by a
+# The behavior of the ADD_SIP_PYTHON_MODULE macro can be controlled by a
 # number of variables:
 #
-# SIP_INCLUDE_DIRS - List of directories which SIP will scan through when looking
+# SIP_INCLUDES - List of directories which SIP will scan through when looking
 #     for included .sip files. (Corresponds to the -I option for SIP.)
 #
 # SIP_TAGS - List of tags to define when running SIP. (Corresponds to the -t
@@ -35,94 +35,170 @@
 # SIP_EXTRA_OPTIONS - Extra command line options which should be passed on to
 #     SIP.
 
-SET(SIP_INCLUDE_DIRS)
+# SIP_BUILD_EXTRA_OPTIONS - Extra command line options which should be passed on to
+#     sip-build.
+
+SET(SIP_INCLUDES)
 SET(SIP_TAGS)
-SET(SIP_CONCAT_PARTS 8)
+SET(SIP_CONCAT_PARTS 16)
 SET(SIP_DISABLE_FEATURES)
 SET(SIP_EXTRA_OPTIONS)
+SET(SIP_EXTRA_OBJECTS)
+SET(SIP_BUILD_EXTRA_OPTIONS)
 
-MACRO(ADD_SIP_PYTHON_MODULE MODULE_NAME MODULE_SIP)
+MACRO(GENERATE_SIP_PYTHON_MODULE_CODE MODULE_NAME MODULE_SIP SIP_FILES CPP_FILES)
 
-    SET(EXTRA_LINK_LIBRARIES ${ARGN})
+  STRING(REPLACE "." "/" _x ${MODULE_NAME})
+  GET_FILENAME_COMPONENT(_parent_module_path ${_x} PATH)
+  GET_FILENAME_COMPONENT(_child_module_name ${_x} NAME)
+  GET_FILENAME_COMPONENT(_module_path ${MODULE_SIP} PATH)
+  GET_FILENAME_COMPONENT(_abs_module_sip ${MODULE_SIP} ABSOLUTE)
 
-    STRING(REPLACE "." "/" _x ${MODULE_NAME})
-    GET_FILENAME_COMPONENT(_parent_module_path ${_x}  PATH)
-    GET_FILENAME_COMPONENT(_child_module_name ${_x} NAME)
+  # If this is not need anymore (using input configuration file for SIP files)
+  # SIP could be run in the source rather than in binary directory
+  SET(_configured_module_sip ${CMAKE_CURRENT_BINARY_DIR}/${_module_path}/${_module_path}.sip)
+  FOREACH (_sip_file ${SIP_FILES})
+    GET_FILENAME_COMPONENT(_sip_file_path ${_sip_file} PATH)
+    GET_FILENAME_COMPONENT(_sip_file_name_we ${_sip_file} NAME_WE)
+    FILE(RELATIVE_PATH _sip_file_relpath ${CMAKE_CURRENT_SOURCE_DIR} "${_sip_file_path}/${_sip_file_name_we}")
+    SET(_out_sip_file "${CMAKE_CURRENT_BINARY_DIR}/${_sip_file_relpath}.sip")
+    CONFIGURE_FILE(${_sip_file} ${_out_sip_file})
+  ENDFOREACH (_sip_file)
 
-    GET_FILENAME_COMPONENT(_module_path ${MODULE_SIP} PATH)
-    GET_FILENAME_COMPONENT(_abs_module_sip ${MODULE_SIP} ABSOLUTE)
+  SET(_message "-DMESSAGE=Generating CPP code for module ${MODULE_NAME}")
+  SET(_sip_output_files)
 
-    # We give this target a long logical target name.
-    # (This is to avoid having the library name clash with any already
-    # install library names. If that happens then cmake dependency
-    # tracking get confused.)
-    STRING(REPLACE "." "_" _logical_name ${MODULE_NAME})
-    SET(_logical_name "python_module_${_logical_name}")
+  # Suppress warnings
+  IF(PEDANTIC)
+    IF(MSVC)
+      ADD_DEFINITIONS(
+        /wd4189  # local variable is initialized but not referenced
+        /wd4996  # deprecation warnings (bindings re-export deprecated methods)
+        /wd4701  # potentially uninitialized variable used (sip generated code)
+        /wd4702  # unreachable code (sip generated code)
+        /wd4703  # potentially uninitialized local pointer variable 'sipType' used
+      )
+    ELSE(MSVC)
+      # disable all warnings
+      ADD_DEFINITIONS( -w -Wno-deprecated-declarations )
+      IF(NOT APPLE)
+        ADD_DEFINITIONS( -fpermissive )
+      ENDIF(NOT APPLE)
+    ENDIF(MSVC)
+  ENDIF(PEDANTIC)
+
+  IF(MSVC)
+    ADD_DEFINITIONS( /bigobj )
+  ENDIF(MSVC)
+
+  IF (SIP_BUILD_EXECUTABLE)
+
+    FILE(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_module_path}/build/${_child_module_name})    # Output goes in this dir.
+
+    FOREACH(CONCAT_NUM RANGE 0 ${SIP_CONCAT_PARTS} )
+      IF( ${CONCAT_NUM} LESS ${SIP_CONCAT_PARTS} )
+        SET(_sip_output_files ${_sip_output_files} ${CMAKE_CURRENT_BINARY_DIR}/${_module_path}/build/${_child_module_name}/sip${_child_module_name}part${CONCAT_NUM}.cpp )
+      ENDIF( ${CONCAT_NUM} LESS ${SIP_CONCAT_PARTS} )
+    ENDFOREACH(CONCAT_NUM RANGE 0 ${SIP_CONCAT_PARTS} )
+
+    SET(SIPCMD ${SIP_BUILD_EXECUTABLE} --no-protected-is-public --pep484-pyi --no-make --concatenate=${SIP_CONCAT_PARTS} --qmake=${QMAKE_EXECUTABLE} --include-dir=${CMAKE_CURRENT_BINARY_DIR} --include-dir=${PYQT5_SIP_DIR} ${SIP_BUILD_EXTRA_OPTIONS})
+
+    ADD_CUSTOM_COMMAND(
+      OUTPUT ${_sip_output_files}
+      COMMAND ${CMAKE_COMMAND} -E echo ${message}
+      COMMAND ${SIPCMD}
+      COMMAND ${CMAKE_COMMAND} -E touch ${_sip_output_files}
+      WORKING_DIRECTORY ${_module_path}
+      MAIN_DEPENDENCY ${_configured_module_sip}
+      DEPENDS ${SIP_EXTRA_FILES_DEPEND}
+      VERBATIM
+    )
+
+  ELSE (SIP_BUILD_EXECUTABLE)
 
     FILE(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/${_module_path})    # Output goes in this dir.
 
     SET(_sip_includes)
     FOREACH (_inc ${SIP_INCLUDES})
-        GET_FILENAME_COMPONENT(_abs_inc ${_inc} ABSOLUTE)
-        LIST(APPEND _sip_includes -I ${_abs_inc})
+      GET_FILENAME_COMPONENT(_abs_inc ${_inc} ABSOLUTE)
+      LIST(APPEND _sip_includes -I ${_abs_inc})
     ENDFOREACH (_inc )
 
     SET(_sip_tags)
     FOREACH (_tag ${SIP_TAGS})
-        LIST(APPEND _sip_tags -t ${_tag})
+      LIST(APPEND _sip_tags -t ${_tag})
     ENDFOREACH (_tag)
 
     SET(_sip_x)
     FOREACH (_x ${SIP_DISABLE_FEATURES})
-        LIST(APPEND _sip_x -x ${_x})
+      LIST(APPEND _sip_x -x ${_x})
     ENDFOREACH (_x ${SIP_DISABLE_FEATURES})
 
-    SET(_message "-DMESSAGE=Generating CPP code for module ${MODULE_NAME}")
-    SET(_sip_output_files)
     FOREACH(CONCAT_NUM RANGE 0 ${SIP_CONCAT_PARTS} )
-        IF( ${CONCAT_NUM} LESS ${SIP_CONCAT_PARTS} )
-            SET(_sip_output_files ${_sip_output_files} ${CMAKE_CURRENT_BINARY_DIR}/${_module_path}/sip${_child_module_name}part${CONCAT_NUM}.cpp )
-        ENDIF( ${CONCAT_NUM} LESS ${SIP_CONCAT_PARTS} )
+      IF( ${CONCAT_NUM} LESS ${SIP_CONCAT_PARTS} )
+        SET(_sip_output_files ${_sip_output_files} ${CMAKE_CURRENT_BINARY_DIR}/${_module_path}/sip${_child_module_name}part${CONCAT_NUM}.cpp )
+      ENDIF( ${CONCAT_NUM} LESS ${SIP_CONCAT_PARTS} )
     ENDFOREACH(CONCAT_NUM RANGE 0 ${SIP_CONCAT_PARTS} )
 
-    # Suppress warnings
-    IF(PEDANTIC)
-      IF(MSVC)
-        # 4996 deprecation warnings (bindings re-export deprecated methods)
-        # 4701 potentially uninitialized variable used (sip generated code)
-        # 4702 unreachable code (sip generated code)
-        ADD_DEFINITIONS( /wd4996 /wd4701 /wd4702 )
-      ELSE(MSVC)
-        # disable all warnings
-        ADD_DEFINITIONS( -w )
-      ENDIF(MSVC)
-    ENDIF(PEDANTIC)
-
+    SET(SIPCMD ${SIP_BINARY_PATH} ${_sip_tags} -w -e ${_sip_x} ${SIP_EXTRA_OPTIONS} -j ${SIP_CONCAT_PARTS} -c ${CMAKE_CURRENT_BINARY_DIR}/${_module_path} -I ${CMAKE_CURRENT_BINARY_DIR}/${_module_path} ${_sip_includes} ${_configured_module_sip})
     ADD_CUSTOM_COMMAND(
-        OUTPUT ${_sip_output_files}
-        COMMAND ${CMAKE_COMMAND} -E echo ${message}
-        COMMAND ${CMAKE_COMMAND} -E touch ${_sip_output_files}
-        COMMAND ${SIP_EXECUTABLE} ${_sip_tags} ${_sip_x} ${SIP_EXTRA_OPTIONS} -j ${SIP_CONCAT_PARTS} -c ${CMAKE_CURRENT_BINARY_DIR}/${_module_path} ${_sip_includes} ${_abs_module_sip}
-        DEPENDS ${_abs_module_sip} ${SIP_EXTRA_FILES_DEPEND}
+      OUTPUT ${_sip_output_files}
+      COMMAND ${CMAKE_COMMAND} -E echo ${message}
+      COMMAND ${CMAKE_COMMAND} -E touch ${_sip_output_files}
+      COMMAND ${SIPCMD}
+      MAIN_DEPENDENCY ${_configured_module_sip}
+      DEPENDS ${SIP_EXTRA_FILES_DEPEND}
+      VERBATIM
     )
-    ADD_LIBRARY(${_logical_name} MODULE ${_sip_output_files} ${SIP_EXTRA_SOURCE_FILES})
-    IF (NOT APPLE)
-        IF ("${Python3_VERSION_MINOR}" GREATER 7)
-            MESSAGE(STATUS "Python > 3.7 - not linking to libpython")
-        ELSE ()
-            TARGET_LINK_LIBRARIES(${_logical_name} ${Python3_LIBRARIES})
-        ENDIF ()
-    ENDIF (NOT APPLE)
-    TARGET_LINK_LIBRARIES(${_logical_name} ${EXTRA_LINK_LIBRARIES})
-    IF (APPLE)
-        SET_TARGET_PROPERTIES(${_logical_name} PROPERTIES LINK_FLAGS "-undefined dynamic_lookup")
-    ENDIF (APPLE)
-    SET_TARGET_PROPERTIES(${_logical_name} PROPERTIES PREFIX "" OUTPUT_NAME ${_child_module_name})
 
-    IF (WIN32)
-        SET_TARGET_PROPERTIES(${_logical_name} PROPERTIES SUFFIX ".pyd" IMPORT_PREFIX "_")
-    ENDIF (WIN32)
+  ENDIF (SIP_BUILD_EXECUTABLE)
 
-    INSTALL(TARGETS ${_logical_name} DESTINATION "${Python3_SITEARCH}/${_parent_module_path}")
+  ADD_CUSTOM_TARGET(generate_sip_${MODULE_NAME}_cpp_files DEPENDS ${_sip_output_files})
 
-ENDMACRO(ADD_SIP_PYTHON_MODULE)
+  SET(CPP_FILES ${sip_output_files})
+ENDMACRO(GENERATE_SIP_PYTHON_MODULE_CODE)
+
+# Will compile and link the module
+MACRO(BUILD_SIP_PYTHON_MODULE MODULE_NAME SIP_FILES EXTRA_OBJECTS)
+  SET(EXTRA_LINK_LIBRARIES ${ARGN})
+
+  # We give this target a long logical target name.
+  # (This is to avoid having the library name clash with any already
+  # install library names. If that happens then cmake dependency
+  # tracking get confused.)
+  STRING(REPLACE "." "_" _logical_name ${MODULE_NAME})
+  SET(_logical_name "python_module_${_logical_name}")
+  GET_FILENAME_COMPONENT(_module_path ${SIP_FILES} PATH)
+
+  ADD_LIBRARY(${_logical_name} MODULE ${_sip_output_files} ${EXTRA_OBJECTS})
+  SET_PROPERTY(TARGET ${_logical_name} PROPERTY AUTOMOC OFF)
+  TARGET_INCLUDE_DIRECTORIES(${_logical_name} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/${_module_path}/build)
+
+  IF (${SIP_VERSION_STR} VERSION_LESS 5.0.0)
+    # require c++14 only -- sip breaks with newer versions due to reliance on throw(...) annotations removed in c++17
+    TARGET_COMPILE_FEATURES(${_logical_name} PRIVATE cxx_std_14)
+  ENDIF (${SIP_VERSION_STR} VERSION_LESS 5.0.0)
+
+  SET_TARGET_PROPERTIES(${_logical_name} PROPERTIES CXX_VISIBILITY_PRESET default)
+  IF (NOT APPLE)
+    TARGET_LINK_LIBRARIES(${_logical_name} ${Python_LIBRARIES})
+  ENDIF (NOT APPLE)
+  TARGET_LINK_LIBRARIES(${_logical_name} ${EXTRA_LINK_LIBRARIES})
+  IF (APPLE)
+    SET_TARGET_PROPERTIES(${_logical_name} PROPERTIES LINK_FLAGS "-undefined dynamic_lookup")
+  ENDIF (APPLE)
+  SET_TARGET_PROPERTIES(${_logical_name} PROPERTIES PREFIX "" OUTPUT_NAME ${_child_module_name})
+
+  IF (WIN32)
+    SET_TARGET_PROPERTIES(${_logical_name} PROPERTIES SUFFIX ".pyd")
+    GET_TARGET_PROPERTY(_runtime_output ${_logical_name} RUNTIME_OUTPUT_DIRECTORY)
+    ADD_CUSTOM_COMMAND(TARGET ${_logical_name} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E echo "Copying extension ${_child_module_name}"
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different "$<TARGET_FILE:${_logical_name}>" "${_runtime_output}/${_child_module_name}.pyd"
+      DEPENDS ${_logical_name}
+      )
+  ENDIF(WIN32)
+
+  INSTALL(TARGETS ${_logical_name} DESTINATION "${Python_SITEARCH}/${_parent_module_path}")
+ENDMACRO(BUILD_SIP_PYTHON_MODULE MODULE_NAME SIP_FILES EXTRA_OBJECTS)
+
