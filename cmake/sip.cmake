5a2fdb428ddc9a35a07be0a0678a16fff6b5b61c
j.spijker@ultimaker.com <jelle spijker>
2022-02-03 17:23:42 +0100

    Clean up
    Contributes to CURA-7924

--- cmake/sip.cmake
+++ cmake/sip.cmake
@@ -3,151 +3,174 @@ set(PRE_BUILD_CMD)
 #set(PRE_BUILD_CMD ". ${CMAKE_BINARY_DIR}/conan/conanbuild.sh && ")
 
 
+macro(get_library_name lib_var)
+    set(lib "${${lib_var}}")
+    if(lib)
+        cmake_path(GET lib STEM LAST_ONLY lib)
+        string(REGEX REPLACE "^lib" "" lib ${lib})
+        list(APPEND _sip_link_libraries ${lib})
+    endif()
+endmacro()
+
+macro(get_library_directory lib_dir_var)
+    set(lib_dir "${${lib_dir_var}}")
+    if(lib_dir)
+        cmake_path(SET lib_dir NORMALIZE "${lib_dir}/")
+        list(APPEND _sip_link_libraries_dirs "${lib_dir}")
+    endif()
+endmacro()
+
+macro(get_library_include_directory lib_dir_var)
+    set(lib_dir "${${lib_dir_var}}")
+    if(lib_dir)
+        cmake_path(SET lib_dir NORMALIZE "${lib_dir}/")
+        list(APPEND _sip_include_dirs "${lib_dir}")
+    endif()
+endmacro()
+
+macro(sanitize_list var_list)
+    list(REMOVE_DUPLICATES ${var_list})
+    list(REMOVE_ITEM ${var_list} "")
+    list(TRANSFORM ${var_list} PREPEND "\"")
+    list(TRANSFORM ${var_list} APPEND "\"")
+    list(JOIN ${var_list} ", " ${var_list})
+endmacro()
+
 function(add_sip_module MODULE_TARGET MODULE_SIP SIP_FILES HDR_FILES SRC_FILES)
     message(STATUS "SIP: Generating files")
+    string(TOUPPER ${CMAKE_BUILD_TYPE} _upper_case_build_type)
 
     # Get the sip modules copy the files to the build and set the tobe generated path.
     # Because that files needs to be in the same folder as the pyproject.toml
     cmake_path(GET MODULE_SIP FILENAME _module_sip_filename)
     cmake_path(GET MODULE_SIP PARENT_PATH _module_sip_path)
+    cmake_path(SET _module_sip_path NORMALIZE "${_module_sip_path}/")
 
     list(PREPEND SIP_FILES ${MODULE_SIP})
+    set(_sip_extra_include_dirs)
+    foreach (_sip_include ${SIP_FILES})
+        cmake_path(GET _sip_include PARENT_PATH _sip_include_path)
+        cmake_path(SET _sip_include_path NORMALIZE "${_sip_include_path}/")
+        list(APPEND _sip_extra_include_dirs ${_sip_include_path})
+    endforeach ()
+    sanitize_list(_sip_extra_include_dirs)
 
     message(STATUS "SIP: Generating pyproject.toml")
     # Get the module specified include locations
-    get_target_property(_sip_inlcude_dirs ${MODULE_TARGET} INTERFACE_INCLUDE_DIRECTORIES)
+    set(_sip_include_dirs)
+    get_target_property(_own_sip_include_dirs ${MODULE_TARGET} INTERFACE_INCLUDE_DIRECTORIES)
+    if(_own_sip_include_dirs)
+        list(APPEND _sip_include_dirs "${_own_sip_include_dirs}")
+    endif()
 
     # Get the module compiler options
-    get_target_property(_compile_features ${MODULE_TARGET} INTERFACE_COMPILE_FEATURES)
-    get_target_property(_compile_options ${MODULE_TARGET} INTERFACE_COMPILE_OPTIONS)
-    get_target_property(_compile_definitions ${MODULE_TARGET} INTERFACE_COMPILE_DEFINITIONS)
+    set(_compile_options)
+    get_target_property(_own_compile_options ${MODULE_TARGET} INTERFACE_COMPILE_OPTIONS)
+    if(_own_compile_options)
+        list(APPEND _compile_options "${_own_compile_options}")
+    endif()
+
+    set(_compile_features)
+    get_target_property(_own_compile_features ${MODULE_TARGET} INTERFACE_COMPILE_FEATURES)
+    if(_own_compile_features)
+        foreach(feature ${_own_compile_features})
+            if(feature STREQUAL "cxx_std_17")
+                list(APPEND _compile_features "-std=c++17")
+            elseif(feature STREQUAL "cxx_std_20")
+                list(APPEND _compile_features "-std=c++20")
+            elseif(feature STREQUAL "cxx_std_23")
+                list(APPEND _compile_features "-std=c++23")
+            endif()
+        endforeach()
+    endif()
+
+    set(_compile_definitions)
+    get_target_property(_own_compile_definitions ${MODULE_TARGET} INTERFACE_COMPILE_DEFINITIONS)
+    if(_own_compile_definitions)
+        list(APPEND _compile_definitions "${_own_compile_definitions}")
+    endif()
 
     # Get the linked dependency include locations and lib names
+    set(_sip_link_libraries)
+    set(_sip_link_libraries_dirs)
+
     get_target_property(_link_libraries ${MODULE_TARGET} INTERFACE_LINK_LIBRARIES)
-    set(_sip_link_libraries Arcus)
-    set(_sip_link_libraries_DIRS)
-    foreach(_link_lib ${_link_libraries})
+    if(_link_libraries)
+        foreach(_link_lib ${_link_libraries})
 
-        get_target_property(_type ${_link_lib} TYPE)
-        if(_type STREQUAL "INTERFACE_LIBRARY")
-            set(_include_directories_prop_var "INTERFACE_INCLUDE_DIRECTORIES")
-            set(_compile_features_prop_var "INTERFACE_COMPILE_FEATURES")
-            set(_compile_options_prop_var "INTERFACE_COMPILE_OPTIONS")
-            set(_compile_definitions_prop_var "INTERFACE_COMPILE_DEFINITIONS")
-        else()
-            set(_include_directories_prop_var "INCLUDE_DIRECTORIES")
-            set(_compile_features_prop_var "COMPILE_FEATURES")
-            set(_compile_options_prop_var "COMPILE_OPTIONS")
-            set(_compile_definitions_prop_var "COMPILE_DEFINITIONS")
-        endif()
+            # Get the name(-space) of the dependency
+            get_target_property(_lib_name ${_link_lib} NAME)
+            string(REPLACE "::" ";" _x "${_lib_name}")
+            list(GET _x 0 _name)
 
-        get_target_property(_link_include_dirs ${_link_lib} ${_include_directories_prop_var})
-        if(_link_include_dirs)
-            list(APPEND _sip_inlcude_dirs ${_link_include_dirs})
-        endif()
+            # Set the property names depending on the dependency type (interface targets need to be named INTERFACE_<>
+            get_target_property(_type ${_link_lib} TYPE)
+            if(_type STREQUAL "INTERFACE_LIBRARY")
+                set(_include_directories_prop_var "INTERFACE_INCLUDE_DIRECTORIES")
+                set(_compile_features_prop_var "INTERFACE_COMPILE_FEATURES")
+                set(_compile_options_prop_var "INTERFACE_COMPILE_OPTIONS")
+                set(_compile_definitions_prop_var "INTERFACE_COMPILE_DEFINITIONS")
+            else()
+                set(_include_directories_prop_var "INCLUDE_DIRECTORIES")
+                set(_compile_features_prop_var "COMPILE_FEATURES")
+                set(_compile_options_prop_var "COMPILE_OPTIONS")
+                set(_compile_definitions_prop_var "COMPILE_DEFINITIONS")
+            endif()
 
-        get_target_property(_lib_compile_features ${_link_lib} ${_compile_features_prop_var})
-        if(_lib_compile_features)
-            list(APPEND _compile_features "${_lib_compile_features}")
-        endif()
+            get_target_property(_lib_compile_features ${_link_lib} ${_compile_features_prop_var})
+            if(_lib_compile_features)
+                foreach(feature ${_lib_compile_features})
+                    if(feature STREQUAL "cxx_std_17")
+                        list(APPEND _compile_features "-std=c++17")
+                    elseif(feature STREQUAL "cxx_std_20")
+                        list(APPEND _compile_features "-std=c++20")
+                    elseif(feature STREQUAL "cxx_std_23")
+                        list(APPEND _compile_features "-std=c++23")
+                    endif()
+                endforeach()
+            endif()
 
-        get_target_property(_lib_compile_options ${_link_lib} ${_compile_options_prop_var})
-        if(_lib_compile_options)
-            list(APPEND _compile_options "${_lib_compile_options}")
-        endif()
+            get_target_property(_lib_compile_options ${_link_lib} ${_compile_options_prop_var})
+            if(_lib_compile_options)
+                list(APPEND _compile_options "${_lib_compile_options}")
+            endif()
 
-        get_target_property(_lib_compile_definitions ${_link_lib} ${_compile_definitions_prop_var})
-        if(_lib_compile_definitions)
-            list(APPEND _compile_definitions "${_lib_compile_definitions}")
-        endif()
+            get_target_property(_lib_compile_definitions ${_link_lib} ${_compile_definitions_prop_var})
+            if(_lib_compile_definitions)
+                list(APPEND _compile_definitions "${_lib_compile_definitions}")
+            endif()
 
-        get_target_property(_lib_name ${_link_lib} NAME)
-        string(REPLACE "::" ";" _x "${_lib_name}")
-        list(GET _x 0 _name)
+            # Get Include directories of dependencies
+            get_target_property(_link_include_dirs ${_link_lib} ${_include_directories_prop_var})
+            if(_link_include_dirs)
+                list(APPEND _sip_include_dirs ${_link_include_dirs})
+            endif()
+            get_library_include_directory("${_name}_INCLUDE_DIRS")
+            get_library_include_directory("${_name}_INCLUDE_DIR")
 
-        string(TOUPPER ${CMAKE_BUILD_TYPE} _upper_case_build_type)
+            # Get library directories
+            get_library_directory("${_name}_LIB_DIRS_${_upper_case_build_type}")
+            get_library_directory("${_name}_LIBRARY_DIRS")
 
-        set(_lib_dir_var "${_name}_LIB_DIRS_${_upper_case_build_type}")
-        set(_lib_dir "${${_lib_dir_var}}")
-        if(_lib_dir)
-            list(APPEND _sip_link_libraries_DIRS "${_lib_dir}")
-        endif()
+            # Get library
+            get_library_name("${_name}_LIBS_${_upper_case_build_type}")
+            get_library_name("${_name}_LIBRARY")
+            get_library_name("${_name}_LIBRARIES")
+            get_library_name("${_name}_SYSTEM_LIBS_${_upper_case_build_type}")
+        endforeach()
+    endif()
 
-        set(_lib_dir_var "${_name}_LIBRARY_DIRS")
-        set(_lib_dir "${${_lib_dir_var}}")
-        if(_lib_dir)
-            list(APPEND _sip_link_libraries_DIRS "${_lib_dir}")
-        endif()
-
-        set(_lib_var "${_name}_LIBS_${_upper_case_build_type}")
-        set(_lib "${${_lib_var}}")
-        if(_lib)
-            cmake_path(GET _lib FILENAME _lib)
-            list(APPEND _sip_link_libraries "${_lib}")
-        endif()
-
-        set(_lib_var "${_name}_LIBRARY")
-        set(_lib "${${_lib_var}}")
-        if(_lib)
-            cmake_path(GET _lib FILENAME _lib)
-            list(APPEND _sip_link_libraries "${_lib}")
-        endif()
-
-        set(_lib_var "${_name}_LIBRARIES")
-        set(_lib "${${_lib_var}}")
-        if(_lib)
-            cmake_path(GET _lib STEM LAST_ONLY _lib)
-            string(REGEX REPLACE "^lib" "" _lib ${_lib})
-            list(APPEND _sip_link_libraries ${_lib})
-        endif()
-
-        set(_lib_var "${_name}_SYSTEM_LIBS_${_upper_case_build_type}")
-        set(_lib "${${_lib_var}}")
-        if(_lib)
-            cmake_path(GET _lib FILENAME _lib)
-            list(APPEND _sip_link_libraries "${_lib}")
-        endif()
-    endforeach()
-
-    # Sanitize the _compile_features
-    list(REMOVE_DUPLICATES _compile_features)
-    string(STRIP "${_compile_features}" _compile_features)
-    string(REPLACE ";" "\", \"" _compile_features "${_compile_features}")
-
-    # Sanitize the _compile_options
-    list(REMOVE_DUPLICATES _compile_options)
-    string(STRIP "${_compile_options}" _compile_options)
-    string(REPLACE ";" "\", \"" _compile_options "${_compile_options}")
-
-    # Sanitize the _compile_definitions
-    list(REMOVE_DUPLICATES _compile_definitions)
-    string(STRIP "${_compile_definitions}" _compile_definitions)
-    string(REPLACE ";" "\", \"" _compile_definitions "${_compile_definitions}")
-
-    # Sanitize the _sip_inlcude_dirs
-    list(REMOVE_DUPLICATES _sip_inlcude_dirs)
-    string(STRIP "${_sip_inlcude_dirs}" _sip_inlcude_dirs)
-    string(REPLACE ";" "\", \"" _sip_inlcude_dirs "${_sip_inlcude_dirs}")
-
-    # Sanitize the _sip_link_libraries
-    list(REMOVE_DUPLICATES _sip_link_libraries)
-    string(STRIP "${_sip_link_libraries}" _sip_link_libraries)
-    string(REPLACE ";" "\", \"" _sip_link_libraries "${_sip_link_libraries}")
-
-    # Sanitize the _sip_link_libraries_DIRS
-    list(REMOVE_DUPLICATES _sip_link_libraries_DIRS)
-    string(STRIP "${_sip_link_libraries_DIRS}" _sip_link_libraries_DIRS)
-    string(REPLACE ";" "\", \"" _sip_link_libraries_DIRS "${_sip_link_libraries_DIRS}")
-
-    # Sanitize the SRC_FILES
-    list(REMOVE_DUPLICATES SRC_FILES)
-    string(STRIP "${SRC_FILES}" SRC_FILES)
-    string(REPLACE ";" "\", \"" SRC_FILES "${SRC_FILES}")
-
-    # Sanitize the HDR_FILES
-    list(REMOVE_DUPLICATES HDR_FILES)
-    string(STRIP "${HDR_FILES}" HDR_FILES)
-    string(REPLACE ";" "\", \"" HDR_FILES "${HDR_FILES}")
+    # Sanitize the lists
+    set(_compile_args)
+    list(APPEND _compile_args ${_compile_features} ${_compile_options})
+    sanitize_list(_compile_args)
+    sanitize_list(_compile_definitions)
+    message(STATUS "BLAAAAAAAAAAAAAAT ${_compile_definitions}")
+    sanitize_list(_sip_include_dirs)
+    sanitize_list(_sip_link_libraries)
+    sanitize_list(_sip_link_libraries_dirs)
+    sanitize_list(SRC_FILES)
+    sanitize_list(HDR_FILES)
 
     file(GENERATE
             OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/pyproject.toml"
@@ -161,21 +184,23 @@ name = \"${MODULE_TARGET}\"
 [tool.sip.project]
 sip-module = \"${_module_sip_filename}\"
 sip-files-dir = \"${_module_sip_path}\"
-sip-include-dirs = [\"${_module_sip_path}\"]
+sip-include-dirs = [${_sip_extra_include_dirs}]
 build-dir = \"${CMAKE_CURRENT_BINARY_DIR}/${MODULE_TARGET}/\"
 verbose = true
 
 [tool.sip.bindings.${MODULE_TARGET}]
-headers = [\"${HDR_FILES}\"]
-include-dirs = [\"${_sip_inlcude_dirs}\"]
-libraries = [\"${_sip_link_libraries}\"]
-library-dirs = [\"${_sip_link_libraries_DIRS}\"]
-sources = [\"${SRC_FILES}\"]
-extra-compile-args = [\"${_compile_options}\"]
-define-macros = []
+headers = [${HDR_FILES}]
+include-dirs = [${_sip_include_dirs}]
+libraries = [${_sip_link_libraries}]
+library-dirs = [${_sip_link_libraries_dirs}]
+sources = [${SRC_FILES}]
+extra-compile-args = [${_compile_args}]
+define-macros = [${_compile_definitions}]
 exceptions = true
-release-gil = true"
-            )
+release-gil = true
+$<$<CONFIG:Debug>:debug = true>
+$<$<BOOL:${BUILD_STATIC}>:static = true>
+")
 
     # Generating the CPP code using sip-build
     set(_sip_output_files)
@@ -198,4 +223,6 @@ release-gil = true"
     )
 
     add_custom_target("sip_${MODULE_TARGET}" ALL DEPENDS ${_sip_output_files})
+
+    # TODO: Installer, for the Python module don't forget about the pyi file for code completion
 endfunction()
